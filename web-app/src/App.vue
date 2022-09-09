bf7dfd5ac3837aa1c49d0f01b1bad16187c35354
Gorka-Lab <user.gorka@gmail.com>
2022-09-09 18:41:36 -0400

    added advertisement top container

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
     <app-bar />
     <v-main>
+      <advertisement-top margin-top="2" margin-bottom="2"></advertisement-top>
       <router-view />
     </v-main>
   </v-app>
@@ -9,11 +10,13 @@
 
 <script>
 import AppBar from "./components/navs/AppBar.vue";
+import AdvertisementTop from "./components/advertisements/AdvertisementTop.vue";
 
 export default {
   name: "App",
   components: {
     AppBar,
+    AdvertisementTop,
   },
   data: () => ({
     //
---
+++ web-app/src/components/advertisements/AdvertisementTop.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container class="my-2">
+    <v-row align="center" justify="center">
+      <v-col cols="10">
+        <v-card elevation="1">
+          <v-card-text> Announcement </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn plain> read more </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ["margin-top", "margin-bottom"],
+};
+</script>
+
+<style></style>
